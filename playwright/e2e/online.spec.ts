8235ae76f8dadbfdbaaf61c19fd7ee513067c268
Otavio Vanzella <vanzella25@gmail.com>
2026-01-31 19:27:31 -0300

    Mudando o nome do cenário

--- playwright/e2e/online.spec.ts
+++ playwright/e2e/online.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('Web App deve estar online', async ({ page }) => {
+test('A Web App deve estar online', async ({ page }) => {
   await page.goto('http://localhost:5173');  
 
   await expect(page).toHaveTitle(/Velô by Papito/);
