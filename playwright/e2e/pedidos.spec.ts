6204d568c3c29da6457f76a70569d3682797a01e
Otavio Vanzella <vanzella25@gmail.com>
2026-01-31 21:23:04 -0300

    add novo cenário

---
+++ playwright/e2e/pedidos.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+test('Deve consultar um pedido aprovado', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+
+  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
+  await page.getByRole('link', { name: 'Consultar Pedido' }).click();
+  await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
+  await page.getByTestId('search-order-id').fill('VLO-3U2B75');
+  await page.getByTestId('search-order-button').click();
+  await expect(page.getByTestId('order-result-id')).toBeVisible();
+  await expect(page.getByTestId('order-result-id')).toContainText('VLO-3U2B75');
+  await expect(page.getByTestId('order-result-status')).toBeVisible();
+  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
+});
